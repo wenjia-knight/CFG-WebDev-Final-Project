1a1e85be8d54f24e8e3e4013998affb59081c9b6
Wenjia Knight <112980096+wk7230545@users.noreply.github.com>
2022-10-17 12:34:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,95 @@
+ul li a {
+    color: white !important;
+}
+
+.sensi_gary {
+    text-decoration: none;
+}
+
+.sensi_gary:hover {
+    text-decoration: none;
+    cursor: pointer;
+}
+
+/* add hover effect to links */
+ul li a:hover {
+    background-color: aliceblue;
+    color: navy !important;
+}
+
+body {
+    background-color: aliceblue;
+    color: navy;
+}
+
+.bg-navy {
+    background-color: navy;
+}
+
+/* Style all font awesome icons */
+
+.fa-facebook {
+    color: white;
+    margin-right: 10px;
+    margin-top: 25px;
+}
+
+.fa-twitter {
+    color: white;
+    margin-right: 10px;
+    margin-top: 25px;
+}
+
+.fa-instagram {
+    color: white;
+    margin-right: 10px;
+    margin-top: 25px;
+}
+
+
+.fa-linkedin-in {
+    color: white;
+    margin-right: 10px;
+    margin-top: 25px;
+}
+
+/* add hover effect to all social media icons */
+
+.fa-facebook:hover {
+    text-decoration: none;
+    opacity: 1;
+}
+
+.fa-twitter:hover {
+    text-decoration: none;
+    opacity: 1;
+}
+
+.fa-instagram:hover {
+    text-decoration: none;
+    opacity: 1;
+}
+
+.fa-linkedin-in:hover {
+    text-decoration: none;
+    opacity: 1;
+}
+
+.btn-navy {
+    color: white;
+    background-color: navy;
+    border-color: white;
+    margin-bottom: 5px;
+}
+
+.btn-navy:hover {
+    color: navy;
+    background-color: aliceblue;
+    border-color: navy;
+}
+
+.back-to-top {
+    position: fixed;
+    bottom: 25px;
+    right: 25px;
+}
