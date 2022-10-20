2a26a833f4804cfda92f36bad97ef6624cc53d69
Wenjia Knight <112980096+wk7230545@users.noreply.github.com>
2022-10-20 21:02:54 +0100

    uploaded the correct JS

--- script.js
+++ script.js
@@ -1,95 +1,16 @@
-ul li a {
-    color: white !important;
-}
-
-.sensi_gary {
-    text-decoration: none;
-}
-
-.sensi_gary:hover {
-    text-decoration: none;
-    cursor: pointer;
-}
-
-/* add hover effect to links */
-ul li a:hover {
-    background-color: aliceblue;
-    color: navy !important;
-}
-
-body {
-    background-color: aliceblue;
-    color: navy;
-}
-
-.bg-navy {
-    background-color: navy;
-}
-
-/* Style all font awesome icons */
-
-.fa-facebook {
-    color: white;
-    margin-right: 10px;
-    margin-top: 25px;
-}
-
-.fa-twitter {
-    color: white;
-    margin-right: 10px;
-    margin-top: 25px;
-}
-
-.fa-instagram {
-    color: white;
-    margin-right: 10px;
-    margin-top: 25px;
-}
-
-
-.fa-linkedin-in {
-    color: white;
-    margin-right: 10px;
-    margin-top: 25px;
-}
-
-/* add hover effect to all social media icons */
-
-.fa-facebook:hover {
-    text-decoration: none;
-    opacity: 1;
-}
-
-.fa-twitter:hover {
-    text-decoration: none;
-    opacity: 1;
-}
-
-.fa-instagram:hover {
-    text-decoration: none;
-    opacity: 1;
-}
-
-.fa-linkedin-in:hover {
-    text-decoration: none;
-    opacity: 1;
-}
-
-.btn-navy {
-    color: white;
-    background-color: navy;
-    border-color: white;
-    margin-bottom: 5px;
-}
-
-.btn-navy:hover {
-    color: navy;
-    background-color: aliceblue;
-    border-color: navy;
-}
-
-.back-to-top {
-    position: fixed;
-    bottom: 25px;
-    right: 25px;
-}
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
